8e93cb7d72d121a513e0ea6b444ca135a168a357
Syed Umair Andrabi <syedumairandrabi66@gmail.com>
2024-09-19 18:49:01 +0530

    add features

--- src/templates/VerticalFeatures.tsx
+++ src/templates/VerticalFeatures.tsx
@@ -7,33 +7,38 @@ const VerticalFeatures = () => (
     description="Stop wasting time on what AI can do better. You focus on what you do best."
   >
     <VerticalFeatureRow
-      title="Minimalistic Advanced AI"
-      description="You ask your question and get the answer from the the most advanced AI out there."
-      image="/assets/images/1.png"
-      imageAlt="AI Chat"
+      title="All trending AI models"
+      description="Access all the trending advanced AI models in one place. From Claude and GPT-4 to Gemini Pro, Flux, Stable Image Ultra, and DALL-E 3 - we've got you covered! 🎭"
+      image="/assets/images/models.png"
+      imageAlt="Model Marketplace"
       imageSmall={true}
     />
-    {/* <VerticalFeatureRow
-      title="Statistics"
-      description="Get detailed insights of the time you spent. Check today's, weekly, monthly study time."
-      image="/assets/images/5.png"
-      imageAlt="Detailed statistics"
-      reverse
-    />
+    
     <VerticalFeatureRow
-      title="Leaderboard"
-      description="Make friends and see who studied most weekly, monthly as well as today. Join friends when they are studying in any room."
-      image="/assets/images/3.png"
-      imageAlt="Detailed leaderboard"
-    />
-    <VerticalFeatureRow
-      title="Distraction Free Solo Study"
-      description="Study distraction free with minimalistic user interface."
-      image="/assets/images/2.png"
-      imageAlt="Minimalistic solo study"
+      title="Conversation to PDF"
+      description="Transform your AI chats into professional-looking PDFs with a click. Edit your text before conversion. With just a click, transform your conversations into polished PDFs. Tweak and perfect your text before conversion, and voila! Imagine resumes, applications, cover letters, and more - all ready in seconds. ✨"
+      image="/assets/images/conversation-to-pdf.png"
+      imageAlt="Conversation to PDF"
       imageSmall={true}
       reverse
-    /> */}
+    />
+    
+    <VerticalFeatureRow
+      title="Creative Spaces"
+      description="Share and explore the most imaginative AI responses in our community spaces. Get inspired daily! 🌈"
+      image="/assets/images/creative-spaces.png"
+      imageAlt="Creative Spaces"
+      imageSmall={true}
+    />
+    
+    <VerticalFeatureRow
+      title="Sleek Interface"
+      description="Navigate with ease through our minimalistic, user-friendly design. 🎨"
+      image="/assets/images/sleek-interface.png"
+      imageAlt="Sleek Interface"
+      imageSmall={true}
+      reverse
+    />
   </Section>
 );
 
